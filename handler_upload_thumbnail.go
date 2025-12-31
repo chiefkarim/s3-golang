508c1e932c7e114a7d241c8d47a4d79899a6c840
chiefkarim <krimotop2016@gmail.com>
2025-12-31 09:34:10 +0100

    feat: upload thumbnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	var maxMemory int64 = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	media, headers, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Please provide thumbnail image", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := headers.Header.Get("Content-type")
+	image, err := io.ReadAll(media)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Please provide thumbnail image", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	if video.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		data:      image,
+		mediaType: mediaType,
+	}
+	videoThumbnails[video.ID] = videoThumbnail
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, video.ID)
+	video.ThumbnailURL = &thumbnailUrl
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
